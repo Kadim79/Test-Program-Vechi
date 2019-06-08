045add7d9177544e6c0ffa2ff35d4fad2b11dedd
Kadim79 <cdimu@icloud.com>
2019-06-08 14:22:42 +0300

    Nimic

---
+++ src/com/sdacademy/core/Main.java
@@ -0,0 +1,56 @@
+package com.sdacademy.core;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // write your code here
+
+        int myNumbers ;
+
+        int i=1;
+        do {
+            System.out.println(i) ;
+            i++;
+
+            if(i<3)
+
+            }
+
+        }
+        while (i<100);
+
+
+/* for(int i=0;i<myNumbers.length;i++)
+        {
+            System.out.println(myNumbers[i]);
+        }
+
+        System.out.println();
+
+//       Next - Enhanced For Statement
+
+        for (int item : myNumbers)
+        {
+            System.out.println(item);
+        }
+
+//       While Statement
+
+        int counter1 = 5;
+
+        while (counter1>0)
+        {
+            System.out.println("Counter = " + counter1);
+            counter1=counter1-1;
+        }
+
+        int counter2 = 100;
+
+        do {
+            System.out.println("\nCounter = " + counter2);
+            counter2++;
+        } while (counter2<0);
+*/
+    }
+}
